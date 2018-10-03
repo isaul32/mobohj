8980afd5928149fea374a929157afe73caf649e3
Sauli Anto <sauli.anto@gmail.com>
2018-10-03 18:03:35 +0300

    Add firebase deps

--- src/exercise7/app/src/main/java/li/sau/exercise7/MainActivity.kt
+++ src/exercise7/app/src/main/java/li/sau/exercise7/MainActivity.kt
@@ -2,11 +2,86 @@ package li.sau.exercise7
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+import jdk.nashorn.internal.runtime.ECMAException.getException
+import org.junit.experimental.results.ResultMatchers.isSuccessful
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+
+
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // DB DEMO
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.getValue(String::class.java)
+                Log.d("DB","Value is: " + value!!)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("DB","Failed to read value.", error.toException())
+            }
+        })
+
+        // AUTH DEMO
+        mAuth = FirebaseAuth.getInstance()
+
+
+        mAuth!!.signInWithEmailAndPassword("test@test.com", "test123")
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("AUTH", "signInWithEmail:success")
+                        val user = mAuth!!.getCurrentUser()
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("AUTH", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(this@MainActivity, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+
+                    // ...
+                }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.currentUser
+        //updateUI(currentUser)
     }
 }
