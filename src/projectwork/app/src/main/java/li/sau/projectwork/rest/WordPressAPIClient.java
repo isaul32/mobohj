67617dd9f99812134ec39e3f598e30952d4cc978
Sauli Anto <sauli.anto@gmail.com>
2019-01-06 21:19:54 +0200

    Add logger for Retrofit

--- src/projectwork/app/src/main/java/li/sau/projectwork/rest/WordPressAPIClient.java
+++ src/projectwork/app/src/main/java/li/sau/projectwork/rest/WordPressAPIClient.java
@@ -3,6 +3,9 @@ package li.sau.projectwork.rest;
 import com.squareup.moshi.Moshi;
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory;
 
+import li.sau.projectwork.BuildConfig;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
@@ -15,11 +18,19 @@ public class WordPressAPIClient {
     static synchronized Retrofit getApiClient() {
         Moshi moshi = new Moshi.Builder().add(new KotlinJsonAdapterFactory()).build();
         if (client == null) {
-            client = new Retrofit
-                    .Builder()
+            Retrofit.Builder builder = new Retrofit.Builder()
                     .baseUrl(BASE_URI)
-                    .addConverterFactory(MoshiConverterFactory.create(moshi))
-                    .build();
+                    .addConverterFactory(MoshiConverterFactory.create(moshi));
+
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+                interceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
+                builder.client(new OkHttpClient.Builder()
+                        .addInterceptor(interceptor)
+                        .build());
+            }
+
+            client = builder.build();
         }
 
         return client;
